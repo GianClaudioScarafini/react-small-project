3b1fe3d540b56eef20ef60337de41581fe899474
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-29 22:50:27 +0100

    feat: Add Intro component to display welcome message

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <section>
+            <h1>Welcome to Gian Claudio website</h1>
+            <p>
+                This is a simple example of a React component.
+            </p>
+        </section>
+    );
+}
+
+export default Intro;
