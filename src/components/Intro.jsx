7925fdda0ae9a24273d1580a055f11b0b5df0a97
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2024-05-31 19:16:02 +0100

    feat: Add react-icons package for using icons in the Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
+import {
+
+    DiReact,
+
+    DiBootstrap,
+
+} from "react-icons/di";
+
+
+
 function Intro() {
     return (
-        <section>
+        <section className="section">
             <h1>Welcome to Gian Claudio website</h1>
             <p>
                 This is a simple example of a React component.
             </p>
+            <h3 className="icons">
+
+                <DiReact />
+                <DiBootstrap />
+            </h3>
         </section>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Intro from './components/Intro.jsx'
 import './index.css'
+import { IconContext } from "react-icons";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Intro />
-    <App />
-  </React.StrictMode>,
 )
